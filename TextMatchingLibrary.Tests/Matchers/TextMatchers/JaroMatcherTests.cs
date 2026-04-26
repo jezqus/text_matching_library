e2579f98fbfed124863d102f1486ea8fa44b3b2a
Jez <piotr.bencki@gmail.com>
2026-04-26 23:53:03 +0200

    Added Jaro matcher

---
+++ TextMatchingLibrary.Tests/Matchers/TextMatchers/JaroMatcherTests.cs
@@ -0,0 +1,33 @@
+﻿
+using TextMatchingLibrary.Matchers;
+
+namespace TextMatchingLibrary.Tests
+{
+    public class JaroMatcherTests
+    {
+        private JaroMatcher jaroMatcher;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.jaroMatcher = new JaroMatcher();
+        }
+
+        [TestCase("frost", "", 0)]
+        [TestCase("", "abc", 0)]
+        [TestCase("forst", "frost", 0.933)]
+        [TestCase("frost", "frost", 1)]
+        [TestCase("frost", "frostXYZ", 0.875)]
+        [TestCase("frostXYZ", "frost", 0.875)]
+        [TestCase("abc", "abcdef", 0.833)]
+        [TestCase("abcdef", "abc", 0.833)]
+        [TestCase("abc", "xyzabcdef", 0.778)]
+        [TestCase("a", "xyzabcdaefaa", 0.694)]
+        public void Match(string first, string second, double expected)
+        {
+            double result = this.jaroMatcher.Match(first, second);
+
+            Assert.That(Math.Round(result, 3), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ TextMatchingLibrary/Matchers/TextMatchers/JaroMatcher.cs
@@ -0,0 +1,47 @@
+﻿using TextMatchingLibrary.Extensions;
+
+namespace TextMatchingLibrary.Matchers
+{
+    public class JaroMatcher : IMatcher<string>
+    {
+        public double Match(string first, string second)
+        {
+            if (first.IsNullOrEmpty() || second.IsNullOrEmpty()) return 0;
+            
+            int maxLength = Math.Max(first.Length, second.Length);
+            int maxDistance = (int)Math.Floor((double)maxLength / 2) - 1;
+
+            int[] matchesInFirst = new int[first.Length];
+            int[] matchesInSecond = new int[second.Length];
+            int matches = 0;
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = Math.Max(0, i - maxDistance); j < Math.Min(second.Length, i + maxDistance + 1); j++)
+                {
+                    if (first[i] == second[j] && matchesInSecond[j] == 0)
+                    {
+                        matchesInFirst[i] = 1;
+                        matchesInSecond[j] = 1;
+                        matches++;
+                        break;
+                    }
+                }
+            }
+
+            int transpositions = 0;
+            int matchesInSecondCursor = 0;
+            for (int i = 0; i < matchesInFirst.Length; i++)
+            {
+                if (matchesInFirst[i] == 1)
+                {
+                    while (matchesInSecond[matchesInSecondCursor] == 0) matchesInSecondCursor++;
+
+                    if (first[i] != second[matchesInSecondCursor++])
+                        transpositions++;
+                }
+            }
+
+            return (((double)matches / (double)first.Length) + ((double)matches / (double)second.Length) + ((double)(matches - (transpositions/2)) / (double)matches)) / 3;
+        }
+    }
+}

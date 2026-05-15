0af04165f3c7b264af7291a6351accbd2ffa7b48
Jez <piotr.bencki@gmail.com>
2026-05-15 02:42:32 +0200

    Added text length matcher

---
+++ TextMatchingLibrary.Tests/Matchers/LengthMatchers/LengthMatcherTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using TextMatchingLibrary.Matchers;
+using TextMatchingLibrary.Normalizers;
+
+namespace TextMatchingLibrary.Tests
+{
+    public class LengthMatcherTests
+    {
+        private LengthMatcher lengthMatcher;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.lengthMatcher = new LengthMatcher();
+        }
+
+        [TestCase("frost", "", 0)]
+        [TestCase("", "abc", 0)]
+        [TestCase("forst", "frost", 1)]
+        [TestCase("frost", "frst", 0.889)]
+        [TestCase("frost", "frostXYZ", 0.769)]
+        [TestCase("frostXYZ", "frost", 0.769)]
+        [TestCase("abc", "abcdef", 0.667)]
+        [TestCase("abcdef", "abc", 0.667)]
+        public void Match(string first, string second, double expected)
+        {
+            double result = this.lengthMatcher.Match(first, second);
+
+            Assert.That(Math.Round(result, 3), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ TextMatchingLibrary/Matchers/LengthMatchers/LengthMatcher.cs
@@ -0,0 +1,16 @@
+﻿using TextMatchingLibrary.Extensions;
+using TextMatchingLibrary.Normalizers;
+
+namespace TextMatchingLibrary.Matchers
+{
+    public class LengthMatcher : IMatcher<string>
+    {
+        public double Match(string first, string second)
+        {
+            if (first.IsNullOrEmpty()) return 0;
+            if (second.IsNullOrEmpty()) return 0;
+
+            return (double)(2 * Math.Min(first.Length, second.Length)) / (double)(first.Length + second.Length);
+        }
+    }
+}
